c1435021ca06b120b2efa91c19b7c6fa5e49b64d
Alex MacOS <tempoh0pe@yahoo.com>
2022-11-21 19:42:21 +0200

    added matches to db

--- football/test.py
+++ football/test.py
@@ -1,6 +1,13 @@
 import requests
 import json
-from models import Scores
+import sys
+sys.path.append("/Users/alekseyiovchev/Python/diplomaMac/diplom/project/")
+print (sys.path)
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','djangoUI.settings')
+import django
+django.setup()
+from football.models import Scores
 
 games_url = 'http://api.football-data.org/v4/matches/?dateFrom=2022-11-11&dateTo=2022-11-21'
 request = requests.get(games_url,headers={'X-Auth-Token':'b5611168bafe4dd2a3fcc7b6b7e19e9a'})
