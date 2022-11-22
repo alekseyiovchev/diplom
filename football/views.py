38df6223c1670d3f89ec42fb37341ff0de351326
Alex MacOS <tempoh0pe@yahoo.com>
2022-11-22 13:56:43 +0200

    configured views and index.html, added matches to main page

--- football/views.py
+++ football/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from football.models import Scores
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'football/html/index.html')
+    return render(request, 'football/html/index.html', { 'data' : Scores.objects.all().order_by('-match_date') })
