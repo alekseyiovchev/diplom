5dfb0542f16e5440598cfcb796b5a87af1a1b9db
Alex MacOS <tempoh0pe@yahoo.com>
2022-12-13 12:15:32 +0200

    edited scripts_main

--- football/scripts_main.py
+++ football/scripts_main.py
@@ -1,19 +1,8 @@
 import scores_main
 import requests
 
-import sys
-sys.path.append("c:\py\diploma\diplomWindows\diplom\project")
-sys.path.append("/Users/alekseyiovchev/Python/diplomaMac/diplom/project/")
-
-import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE','djangoUI.settings')
-
-import django
-django.setup()
-
-
-import requests
 from football.models import Scores,Players
+
 
 games_url = 'http://api.football-data.org/v4/matches/?dateFrom=2022-11-05&dateTo=2022-11-12'
 request = requests.get(games_url,headers={'X-Auth-Token':'b5611168bafe4dd2a3fcc7b6b7e19e9a'})
