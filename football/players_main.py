8a2f7cec4c67519e98e4003395137f5795f45aec
Alex MacOS <tempoh0pe@yahoo.com>
2022-12-13 12:23:52 +0200

    scipts edited

--- football/players_main.py
+++ football/players_main.py
@@ -10,33 +10,33 @@ django.setup()
 
 
 import requests
-from football.models import Scores
+from football.models import Players
 
-id = 44
-games_url = 'http://api.football-data.org/v4/matches/?dateFrom=2022-11-11&dateTo=2022-11-21'
-request = requests.get(games_url,headers={'X-Auth-Token':'b5611168bafe4dd2a3fcc7b6b7e19e9a'})
-result = request.json()['matches']
-for match in result:
-    print(
-        match['id'],
-        match['utcDate'],
-        match['homeTeam']['shortName'],
-        match['homeTeam']['crest'],
-        '|',
-        match['awayTeam']['shortName'],
-        match['awayTeam']['crest'],
-        match['score']['fullTime']['home'],
-        '-',
-        match['score']['fullTime']['away']
-        )
-    
-    Scores.objects.create(
-        id=match['id'],
-        first_team=match['homeTeam']['shortName'],
-        first_team_icon = match['homeTeam']['crest'],
-        second_team=match['awayTeam']['shortName'],
-        second_team_icon = match['awayTeam']['crest'],
-        match_date=match['utcDate'],
-        score_first_team=int(match['score']['fullTime']['home']),
-        score_second_team=int(match['score']['fullTime']['away'])
-        )
+
+def run_players(result):
+    for data in result:
+        print(Players.objects.filter(id=data['id']))
+        if not Players.objects.filter(id=data['id']):
+            Players.objects.create(
+                id=data['id'],
+                first_team=data['homeTeam']['shortName'],
+                first_team_icon = data['homeTeam']['crest'],
+                second_team=data['awayTeam']['shortName'],
+                second_team_icon = data['awayTeam']['crest'],
+                data_date=data['utcDate'],
+                score_first_team=int(data['score']['fullTime']['home']),
+                score_second_team=int(data['score']['fullTime']['away'])
+                )
+            
+            print(
+            data['id'],
+            data['utcDate'],
+            data['homeTeam']['shortName'],
+            data['homeTeam']['crest'],
+            '|',
+            data['awayTeam']['shortName'],
+            data['awayTeam']['crest'],
+            data['score']['fullTime']['home'],
+            '-',
+            data['score']['fullTime']['away']
+            )
--- football/scripts_main.py
+++ football/scripts_main.py
@@ -3,11 +3,22 @@ import requests
 
 from football.models import Scores,Players
 
+host = 'api.football-data.org/v4'
+api = {'X-Auth-Token':'b5611168bafe4dd2a3fcc7b6b7e19e9a'}
 
-games_url = 'http://api.football-data.org/v4/matches/?dateFrom=2022-11-05&dateTo=2022-11-12'
-request = requests.get(games_url,headers={'X-Auth-Token':'b5611168bafe4dd2a3fcc7b6b7e19e9a'})
-result = request.json()['matches']
+# Scores
+games_url = f'http://{host}/matches/?dateFrom=2022-11-05&dateTo=2022-11-12'
 
+# Players
+id = 44
+player_url = f'https://{host}/persons/{id}/matches?status=FINISHED'
+
+
+def get_info(url):
+    request = requests.get(url,headers=api)
+    result = request.json()['matches']
+    return result
+    
 
 if __name__ == "__main__":
-    scores_main.run_scores(result)
+    scores_main.run_scores(get_info(games_url))
