bbc0763073fc41ac117c55b7bb7a2ba1bc212917
Alex MacOS <tempoh0pe@yahoo.com>
2022-12-13 12:05:38 +0200

    edited scripts

--- football/scores_main.py
+++ football/scores_main.py
@@ -15,7 +15,20 @@ from football.models import Scores
 
 def run_scores(result):
     for match in result:
-        print(
+        print(Scores.objects.filter(id=match['id']))
+        if not Scores.objects.filter(id=match['id']):
+            Scores.objects.create(
+                id=match['id'],
+                first_team=match['homeTeam']['shortName'],
+                first_team_icon = match['homeTeam']['crest'],
+                second_team=match['awayTeam']['shortName'],
+                second_team_icon = match['awayTeam']['crest'],
+                match_date=match['utcDate'],
+                score_first_team=int(match['score']['fullTime']['home']),
+                score_second_team=int(match['score']['fullTime']['away'])
+                )
+            
+            print(
             match['id'],
             match['utcDate'],
             match['homeTeam']['shortName'],
@@ -26,15 +39,4 @@ def run_scores(result):
             match['score']['fullTime']['home'],
             '-',
             match['score']['fullTime']['away']
-            )
-        
-        Scores.objects.create(
-            id=match['id'],
-            first_team=match['homeTeam']['shortName'],
-            first_team_icon = match['homeTeam']['crest'],
-            second_team=match['awayTeam']['shortName'],
-            second_team_icon = match['awayTeam']['crest'],
-            match_date=match['utcDate'],
-            score_first_team=int(match['score']['fullTime']['home']),
-            score_second_team=int(match['score']['fullTime']['away'])
             )
