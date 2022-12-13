0475bbe64305c707e7d628bbbea82e3de943ef7f
Alex MacOS <tempoh0pe@yahoo.com>
2022-12-13 12:33:21 +0200

    edited db

--- football/script.py
+++ football/script.py
@@ -2,9 +2,9 @@ import requests
 import json
 from .models import Scores
 
-games_url = 'http://api.football-data.org/v4/matches/?dateFrom=2022-11-10&dateTo=2022-11-17'
+games_url = 'https://api.football-data.org/v4/persons/44/matches?status=FINISHED'
 request = requests.get(games_url,headers={'X-Auth-Token':'b5611168bafe4dd2a3fcc7b6b7e19e9a'})
 result = request.json()['matches']
 for i in result:
     Scores.objects.create()
-    print(i['id'],i['utcDate'],i['homeTeam']['shortName'],'|',i['awayTeam']['shortName'],i['score']['fullTime']['home'],'-',i['score']['fullTime']['away'])
+    print(i['person']['id'],i['utcDate'],i['homeTeam']['shortName'],'|',i['awayTeam']['shortName'],i['score']['fullTime']['home'],'-',i['score']['fullTime']['away'])
